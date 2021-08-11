2e2851ce47e012ecff05def1fc0edca1c3ad9d59
rkabiljo <51742526+rkabiljo@users.noreply.github.com>
2021-08-11 16:43:53 +0100

    Update assembleAndRepeatMasker.py

--- pythonScripts/assembleAndRepeatMasker.py
+++ pythonScripts/assembleAndRepeatMasker.py
@@ -8,6 +8,7 @@ print ("Writing to file " + sys.argv[2])
 bamFile = sys.argv[3]
 outDir = sys.argv[4]
 scriptsDir=sys.argv[6]
+element=sys.argv[7]
 repeatMaskerPath = sys.argv[5]
 for line in inFile:
     line = line.rstrip()
@@ -17,7 +18,6 @@ for line in inFile:
     os.system("python %s/FastqToFastaAndQual.py %s/%s" %(scriptsDir,outDir,line))
     os.system("cap3 %s/%s.fasta  > %s/%s.log" %(outDir,line,outDir,line))
     os.system("perl %sRepeatMasker %s/%s.fasta.cap.contigs -pa 2 -dir %s/repeatmasker" %(repeatMaskerPath,outDir,line, outDir)) 
-    os.system("python %s/testForHervPresence.py %s/repeatmasker/%s.fasta.cap.contigs.out >> %s" %(scriptsDir,outDir,line,sys.argv[2]))
+    os.system("python %s/testForHervPresence.py %s/repeatmasker/%s.fasta.cap.contigs.out %s >> %s" %(scriptsDir,outDir,line,element,sys.argv[2]))
 
 inFile.close()
-
