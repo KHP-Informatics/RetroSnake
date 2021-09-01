0c9dae761704194fe9c34cd44bb3dfc25ebba1f9
rkabiljo <51742526+rkabiljo@users.noreply.github.com>
2021-09-01 12:38:15 +0100

    Update assembleAndRepeatMasker.py

--- pythonScripts/assembleAndRepeatMasker.py
+++ pythonScripts/assembleAndRepeatMasker.py
@@ -4,22 +4,25 @@ import sys
 
 inFile = open(sys.argv[1],"r")
 
-#print ("Writing to file " + sys.argv[2])
-bamFile = sys.argv[3]
-outDir = sys.argv[4]
-scriptsDir=sys.argv[6]
-element=sys.argv[7]
-repeatMaskerPath = sys.argv[5]
-verificationLevel=sys.argv[8]
+bamFile = sys.argv[2]
+parts= bamFile.split('/')
+bam=parts[-1][0:(len(parts[-1])-4)]
+outDir = sys.argv[3]
+outDirRep = outDir + "/" + bam
+os.system("mkdir -p %s" %(outDirRep))
+scriptsDir=sys.argv[5]
+element=sys.argv[6]
+repeatMaskerPath = sys.argv[4]
+verificationLevel=sys.argv[7]
+outFile=sys.argv[8]
 for line in inFile:
     line = line.rstrip()
-##    print (line)
-    os.system("samtools view -b %s -o  %s/%s.bam %s" %(bamFile,outDir,line,line))
-    os.system("samtools bam2fq %s/%s.bam > %s/%s.fq" %(outDir,line,outDir,line))
-    os.system("python %s/FastqToFastaAndQual.py %s/%s" %(scriptsDir,outDir,line))
-    os.system("cap3 %s/%s.fasta  > %s/%s.log" %(outDir,line,outDir,line))
-    os.system("perl %sRepeatMasker %s/%s.fasta.cap.contigs -pa 2 -dir %s/repeatmasker" %(repeatMaskerPath,outDir,line, outDir)) 
-    os.system("python %s/testForHervPresence.py %s/repeatmasker/%s.fasta.cap.contigs.out %s %s >> %s" %(scriptsDir,outDir,line,element,verificationLevel,sys.argv[2]))
+    os.system("samtools view -b %s -o  %s/%s.bam %s" %(bamFile,outDirRep,line,line))
+    os.system("samtools bam2fq %s/%s.bam > %s/%s.fq" %(outDir,line,outDirRep,line))
+    os.system("python %s/FastqToFastaAndQual.py %s/%s" %(scriptsDir,outDirRep,line))
+    os.system("cap3 %s/%s.fasta  > %s/%s.log" %(outDirRep,line,outDirRep,line))
+    os.system("perl %sRepeatMasker %s/%s.fasta.cap.contigs -pa 2 -dir %s/repeatmasker" %(repeatMaskerPath,outDirRep,line, outDirRep)) 
+    os.system("python %s/testForHervPresence.py %s/repeatmasker/%s.fasta.cap.contigs.out %s %s >> %s" %(scriptsDir,outDirRep,line,element,verificationLevel,outFile))
 
 inFile.close()
 
