0bb28777dd38ad5be0ddde1f2573bc598fc5cfa2
rkabiljo <51742526+rkabiljo@users.noreply.github.com>
2021-09-13 17:29:45 +0100

    Update assembleAndRepeatMasker.py

--- pythonScripts/assembleAndRepeatMasker.py
+++ pythonScripts/assembleAndRepeatMasker.py
@@ -21,8 +21,8 @@ for line in inFile:
     os.system("samtools bam2fq %s/%s.bam > %s/%s.fq" %(outDirRep,line,outDirRep,line))
     os.system("python %s/FastqToFastaAndQual.py %s/%s" %(scriptsDir,outDirRep,line))
     os.system("cap3 %s/%s.fasta  > %s/%s.log" %(outDirRep,line,outDirRep,line))
-    #os.system("perl %sRepeatMasker %s/%s.fasta.cap.contigs -pa 2 -dir %s/repeatmasker" %(repeatMaskerPath,outDirRep,line, outDirRep)) 
-    #os.system("python %s/testForHervPresence.py %s/repeatmasker/%s.fasta.cap.contigs.out %s %s >> %s" %(scriptsDir,outDirRep,line,element,verificationLevel,outFile))
+    os.system("perl %sRepeatMasker %s/%s.fasta.cap.contigs -pa 2 -dir %s/repeatmasker" %(repeatMaskerPath,outDirRep,line, outDirRep)) 
+    os.system("python %s/testForHervPresence.py %s/repeatmasker/%s.fasta.cap.contigs.out %s %s >> %s" %(scriptsDir,outDirRep,line,element,verificationLevel,outFile))
 
 inFile.close()
 
